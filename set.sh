c97edc19160588afc67703b2e82430069de32f2c
sumanthkumar113 <99758293+sumanthkumar113@users.noreply.github.com>
2024-10-15 11:14:01 +0530

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Setting up environment..."
-export COMPOSIO_API_KEY=6rg3fwec67se4ll2j7g25o
-export OPENAI_API_KEY=sk-L2YkXNTz_cYfwZ0g9KS_ucPlGxrMgTgI0Y0LOWzvNZT3BlbkFJT8G1we_6FQ__jJ5AcV578waWAUH6hLPDvU0iRmPGcA
+export COMPOSIO_API_KEY=api_key
+export OPENAI_API_KEY=appi+key
 echo "Environment setup complete!"
